b6e44876f1dc32ab1c7b5d2f20ff47f473393a39
Rafael Soares <r.soares2015@outlook.com>
2022-10-24 23:03:31 -0300

    Estilização da página

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 import useRegistration from "../hooks/useRegistration";
+import "../styles/Registration.css";
 
 export default function Registration() {
   const {
@@ -15,66 +16,51 @@ export default function Registration() {
     redirect,
   } = useRegistration();
 
-  if (redirect.length > 0) return (<Redirect to={redirect} />);
+  if (redirect.length > 0) return <Redirect to={redirect} />;
   return (
-    <section>
+    <section className="registration-container">
       <h3>Bem vindo(a) ao</h3>
       <img src="" alt="" />
       <strong>Vamos criar a sua conta</strong>
-      <form>
-        <label htmlFor="name">
-          Nome:
-          <input
-            type="text"
-            name="name"
-            placeholder="Digite seu nome"
-            onChange={handleChangeName}
-          />
-        </label>
-        <label htmlFor="image">
-          Imagem:
-          <input
-            type="text"
-            name="image"
-            placeholder="Use uma imagem de qualquer rede social"
-            onChange={handleChangeImage}
-          />
-        </label>
-        <label htmlFor="email">
-          E-mail:
-          <input
-            type="email"
-            name="email"
-            placeholder="Digite seu e-mail"
-            onChange={handleChangeEmail}
-          />
-        </label>
-        <label htmlFor="password">
-          Senha:
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-            onChange={handleChangePassword}
-          />
-        </label>
-        <label htmlFor="confirmPassword">
-          Confirme a senha:
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Digite novamente sua senha"
-            onChange={handleChangeConfirmPassword}
-          />
-        </label>
-        <button
-          type="button"
-          disabled={!buttonDisabled}
-          onClick={checkPasswords}
-        >
-          Criar Conta
-        </button>
-      </form>
+      <hr />
+      <input
+        type="text"
+        name="name"
+        placeholder="Digite seu nome"
+        onChange={handleChangeName}
+      />
+      <input
+        type="text"
+        name="image"
+        placeholder="Use uma imagem de qualquer rede social"
+        onChange={handleChangeImage}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Digite seu e-mail"
+        onChange={handleChangeEmail}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Digite sua senha"
+        onChange={handleChangePassword}
+      />
+      <input
+        type="password"
+        name="confirmPassword"
+        placeholder="Digite novamente sua senha"
+        onChange={handleChangeConfirmPassword}
+      />
+      <button
+        type="button"
+        disabled={!buttonDisabled}
+        onClick={checkPasswords}
+        className="btn-new-user"
+      >
+        Criar Conta
+      </button>
     </section>
   );
 }
