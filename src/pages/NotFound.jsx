483976dc5d88a31b169832f6bef5e1d6893c518f
Rafael Soares <r.soares2015@outlook.com>
2022-11-26 21:15:43 -0300

    chore: Finalizando a tela NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom'
 export default function NotFound() {
   return (
     <section>
+      <div>
+        <h1>404</h1>
+      </div>
+      <div>
         <h1>Oops, não conseguimos encontrar essa página</h1>
         <Link to="/feed">Tente voltar para o Feed</Link>
+      </div>
     </section>
   )
 }
